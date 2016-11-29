a647205f78a792c517327c99764b1175f22515da
BacksLove <t.boubakar@outlook.com>
2016-11-29 15:05:51 +0100

    Mise a jour des fichiers Container et Tache. Boubakar

---
+++ Mediametrie/Container.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace Mediametrie
+{
+	public class Container
+	{
+		int id;
+		string Nom;
+		int Etat;
+
+		private void NewContainer()
+		{
+			id = 0;
+			Nom = "";
+			Etat = 0;
+		}
+
+		private void NewContainer(int i, string n, int e)
+		{
+			id = i;
+			Nom = n;
+			Etat = e;
+		}
+
+		private void SetId(int i)
+		{
+			id = i;
+		}
+
+		private void SetNom(string n)
+		{
+			Nom = n;
+		}
+
+		private void SetEtat(int e)
+		{
+			Etat = e;
+		}
+
+		private int GetId()
+		{
+			return id;
+		}
+
+		private string GetNom()
+		{
+			return Nom;
+		}
+
+		private int GetEtat()
+		{
+			return Etat;
+		}
+	}
+}
---
+++ Mediametrie/Tache.cs
@@ -0,0 +1,120 @@
+﻿using System;
+namespace Mediametrie
+{
+	public class Tache
+	{
+		int id;
+		int idContainer;
+		string Nom;
+		string Description;
+		DateTime Date_de_creation;
+		DateTime Date_de_fin;
+		byte Priorite;
+		bool Etat;
+
+		private void NewTache()
+		{
+			id = 0;
+			idContainer = 0;
+			Nom = "";
+			Description = "";
+			Date_de_creation = DateTime.Now;
+			Date_de_fin = DateTime.Now;
+			Priorite = 0;
+			Etat = true;
+		}
+
+		private void NewTache(int i, int ic, string n, string d, DateTime df, byte p, bool e)
+		{
+			id = i;
+			idContainer = ic;
+			Nom = n;
+			Description = d;
+			Date_de_creation = DateTime.Now;
+			Date_de_fin = df;
+			Priorite = p;
+			Etat = e;
+		}
+
+		private void SetId(int i)
+		{
+			id = i;
+		}
+
+		private void SetIdContainer(int ic)
+		{
+			idContainer = ic;
+		}
+
+		private void SetNom(string n)
+		{
+			Nom = n;
+		}
+
+		private void SetDescription(string d)
+		{
+			Description = d;
+		}
+
+		private void SetDate_de_fin(DateTime df)
+		{
+			Date_de_fin = df;
+		}
+
+		private void SetDate_de_creation()
+		{
+			Date_de_creation = DateTime.Now;
+		}
+
+		private void SetPriorite(byte p)
+		{
+			Priorite = p;
+		}
+
+		private void SetEtat(bool e)
+		{
+			Etat = e;
+		}
+
+		private int GetId()
+		{
+			return id;
+		}
+
+		private int GetIdContainer()
+		{
+			return idContainer;
+		}
+
+		private string GetNom()
+		{
+			return Nom;
+		}
+
+		private string GetDescription()
+		{
+			return Description;
+		}
+
+		private DateTime GetDate_de_fin()
+		{
+			return Date_de_fin;
+		}
+
+		private DateTime GetDate_de_creation()
+		{
+			return Date_de_creation;
+		}
+
+		private byte GetPriorite()
+		{
+			return Priorite;
+		}
+
+		private bool GetEtat()
+		{
+			return Etat;
+		}
+
+	}
+}
