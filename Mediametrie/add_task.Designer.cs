199184a5d064cffd72f6e0da091a521538dee495
Gary <Gary@DESKTOP-D177ID3>
2016-11-30 11:42:07 +0100

    Amelioration formulaire d'ajout

--- Mediametrie/add_task.Designer.cs
+++ Mediametrie/add_task.Designer.cs
@@ -32,17 +32,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.description = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.end_date = new System.Windows.Forms.DateTimePicker();
+            this.radio_prio1 = new System.Windows.Forms.RadioButton();
+            this.radio_prio2 = new System.Windows.Forms.RadioButton();
+            this.radio_prio4 = new System.Windows.Forms.RadioButton();
+            this.radio_prio3 = new System.Windows.Forms.RadioButton();
+            this.radio_prio5 = new System.Windows.Forms.RadioButton();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_val = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -82,14 +82,14 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Description";
             // 
-            // textBox2
+            // description
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox2.Location = new System.Drawing.Point(12, 133);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(644, 195);
-            this.textBox2.TabIndex = 3;
+            this.description.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.description.Location = new System.Drawing.Point(12, 133);
+            this.description.Multiline = true;
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(644, 195);
+            this.description.TabIndex = 3;
             // 
             // label4
             // 
@@ -101,65 +101,65 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "Date de fin";
             // 
-            // dateTimePicker1
+            // end_date
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.dateTimePicker1.Location = new System.Drawing.Point(367, 60);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(289, 35);
-            this.dateTimePicker1.TabIndex = 6;
+            this.end_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.end_date.Location = new System.Drawing.Point(367, 60);
+            this.end_date.Name = "end_date";
+            this.end_date.Size = new System.Drawing.Size(289, 35);
+            this.end_date.TabIndex = 6;
             // 
-            // radioButton1
+            // radio_prio1
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Checked = true;
-            this.radioButton1.Location = new System.Drawing.Point(12, 392);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(43, 24);
-            this.radioButton1.TabIndex = 7;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radio_prio1.AutoSize = true;
+            this.radio_prio1.Checked = true;
+            this.radio_prio1.Location = new System.Drawing.Point(12, 392);
+            this.radio_prio1.Name = "radio_prio1";
+            this.radio_prio1.Size = new System.Drawing.Size(43, 24);
+            this.radio_prio1.TabIndex = 7;
+            this.radio_prio1.TabStop = true;
+            this.radio_prio1.Text = "1";
+            this.radio_prio1.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // radio_prio2
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(61, 392);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(43, 24);
-            this.radioButton2.TabIndex = 8;
-            this.radioButton2.Text = "2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radio_prio2.AutoSize = true;
+            this.radio_prio2.Location = new System.Drawing.Point(61, 392);
+            this.radio_prio2.Name = "radio_prio2";
+            this.radio_prio2.Size = new System.Drawing.Size(43, 24);
+            this.radio_prio2.TabIndex = 8;
+            this.radio_prio2.Text = "2";
+            this.radio_prio2.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // radio_prio4
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(159, 392);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(43, 24);
-            this.radioButton3.TabIndex = 10;
-            this.radioButton3.Text = "4";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radio_prio4.AutoSize = true;
+            this.radio_prio4.Location = new System.Drawing.Point(159, 392);
+            this.radio_prio4.Name = "radio_prio4";
+            this.radio_prio4.Size = new System.Drawing.Size(43, 24);
+            this.radio_prio4.TabIndex = 10;
+            this.radio_prio4.Text = "4";
+            this.radio_prio4.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // radio_prio3
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(110, 392);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(43, 24);
-            this.radioButton4.TabIndex = 9;
-            this.radioButton4.Text = "3";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radio_prio3.AutoSize = true;
+            this.radio_prio3.Location = new System.Drawing.Point(110, 392);
+            this.radio_prio3.Name = "radio_prio3";
+            this.radio_prio3.Size = new System.Drawing.Size(43, 24);
+            this.radio_prio3.TabIndex = 9;
+            this.radio_prio3.Text = "3";
+            this.radio_prio3.UseVisualStyleBackColor = true;
             // 
-            // radioButton6
+            // radio_prio5
             // 
-            this.radioButton6.AutoSize = true;
-            this.radioButton6.Location = new System.Drawing.Point(208, 392);
-            this.radioButton6.Name = "radioButton6";
-            this.radioButton6.Size = new System.Drawing.Size(43, 24);
-            this.radioButton6.TabIndex = 11;
-            this.radioButton6.Text = "5";
-            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radio_prio5.AutoSize = true;
+            this.radio_prio5.Location = new System.Drawing.Point(208, 392);
+            this.radio_prio5.Name = "radio_prio5";
+            this.radio_prio5.Size = new System.Drawing.Size(43, 24);
+            this.radio_prio5.TabIndex = 11;
+            this.radio_prio5.Text = "5";
+            this.radio_prio5.UseVisualStyleBackColor = true;
             // 
             // label5
             // 
@@ -181,14 +181,15 @@
             this.label6.TabIndex = 13;
             this.label6.Text = "1 : Faible -> 5 : Forte";
             // 
-            // button1
+            // btn_val
             // 
-            this.button1.Location = new System.Drawing.Point(535, 352);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 75);
-            this.button1.TabIndex = 14;
-            this.button1.Text = "Valider";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_val.Location = new System.Drawing.Point(535, 352);
+            this.btn_val.Name = "btn_val";
+            this.btn_val.Size = new System.Drawing.Size(121, 75);
+            this.btn_val.TabIndex = 14;
+            this.btn_val.Text = "Valider";
+            this.btn_val.UseVisualStyleBackColor = true;
+            this.btn_val.Click += new System.EventHandler(this.btn_val_Click);
             // 
             // button2
             // 
@@ -198,6 +199,7 @@
             this.button2.TabIndex = 15;
             this.button2.Text = "Reset";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // add_task
             // 
@@ -205,18 +207,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(678, 456);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_val);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.radioButton6);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.radio_prio5);
+            this.Controls.Add(this.radio_prio4);
+            this.Controls.Add(this.radio_prio3);
+            this.Controls.Add(this.radio_prio2);
+            this.Controls.Add(this.radio_prio1);
+            this.Controls.Add(this.end_date);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.description);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.title);
@@ -233,17 +235,17 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox description;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.DateTimePicker end_date;
+        private System.Windows.Forms.RadioButton radio_prio1;
+        private System.Windows.Forms.RadioButton radio_prio2;
+        private System.Windows.Forms.RadioButton radio_prio4;
+        private System.Windows.Forms.RadioButton radio_prio3;
+        private System.Windows.Forms.RadioButton radio_prio5;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_val;
         private System.Windows.Forms.Button button2;
     }
 }
--- Mediametrie/add_task.cs
+++ Mediametrie/add_task.cs
@@ -16,5 +16,35 @@ namespace Mediametrie
         {
             InitializeComponent();
         }
+
+        private void btn_val_Click(object sender, EventArgs e)
+        {
+            string descript = description.Text;
+            string titre = title.Text;
+            DateTime date = end_date.Value;
+            int prio = 0;
+            if (radio_prio1.Checked)
+                prio = Int32.Parse(radio_prio1.Text);
+            else if (radio_prio2.Checked)
+                prio = Int32.Parse(radio_prio2.Text);
+            else if (radio_prio3.Checked)
+                prio = Int32.Parse(radio_prio3.Text);
+            else if (radio_prio4.Checked)
+                prio = Int32.Parse(radio_prio4.Text);
+            else if (radio_prio5.Checked)
+                prio = Int32.Parse(radio_prio5.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            description.Text = "";
+            title.Text = "";
+            end_date.Value = DateTime.Now;
+            radio_prio1.Checked = true;
+            radio_prio2.Checked = false;
+            radio_prio3.Checked = false;
+            radio_prio4.Checked = false;
+            radio_prio5.Checked = false;
+        }
     }
 }
